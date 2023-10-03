8f5d0cb1483cc15e419ab89effb6952bce7d8340
Ulises Farias <Ulisesfarias1905@hotmail.com>
2023-10-03 10:47:34 -0300

    agregado de carpeta public,views,y archivo app

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3030
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')));
+app.get('/babage',(req,res) => res.sendFile(path.join(__dirname,'views','babage.html')));
+app.get('/berners-lee.html',(req,res) => res.sendFile(path.join(__dirname,'views','berners-lee.html')));
+app.get('/clarke',(req,res) => res.sendFile(path.join(__dirname,'views','clarke.html')));
+app.get('/hamilton',(req,res) => res.sendFile(path.join(__dirname,'views','hamilton.html')));
+app.get('/hopper',(req,res) => res.sendFile(path.join(__dirname,'views','hopper.html')));
+app.get('/lovelace',(req,res) => res.sendFile(path.join(__dirname,'views','lovelace.html')));
+app.get('/turing',(req,res) => res.sendFile(path.join(__dirname,'views','turing.html')));
+
+
+app.listen(PORT,()=> console.log(`Server running in http://localhost:${PORT}`));
